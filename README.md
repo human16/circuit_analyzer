81b6013078eff88b038fc0e49ab12a3bf3693b7d
yesIAmHuman <50881769+human16@users.noreply.github.com>
2025-10-19 17:30:10 -0400

    added bitwise opperations for some ungodly reason

--- README.md
+++ README.md
@@ -1,19 +1,64 @@
+# Sections:
+[Functions](#functions)
+
+[Data Type](#data-type)
+
+# Data Type
+
+In this simulation, instead of only considering `1` and `0`, we need to also consider `z` and `x` (high impedence and variable value), to do this, along with considering `1` and `0`, we will use a bitwise operation on a 4 bit number `n`
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`z x 1 0`<br>
+`n = _ _ _ _` where `n` is our number and we can use bitwise operations to check if a value is `true` or `false`
+
+Example:
+Say we want to express the set of `{z, 1, 0}`, this would make our `n = 1011`. Now, we can convert to base-10, where we would have `n = 11`.
+
+
 # Functions:
+[Transitor](#transistor-functions)
 
-## Transistor functions
+[Gate Logic](#gate-logic-functions)
 
-These functions takes in one variable `input` which is expected to be from the set {`"0"`, `"1"`, `"x"`}
+## Transistor Functions
 
-The functions can return the tuples `("0")`, `("1")`, or `("0", "1")`
+[pmos](#pmos_transistor)
 
-The following assumptions are made:
-* if `input == x` the function will return the result for input `"0"` and `"1"`, which will always be the tuple `("0", "1")`
+[nmos](#nmos_transistor)
+
+These functions takes in one variable `input` which is expected to be from the set {`0`, `1`, `x`}
+
+
+The following assumptions is made:
 
 * `input` cannot be `z`, because if it is, the gate testing stage will supply the previous `input` (which is the assumption for `z` as a transistor "input")
 
 ### pmos_transistor()
 Imitates the pmos logic
 
-### noms_transistor()
+Since pmos transistors are always connected to VDD (1), we can make assumptions about their logic
+
+The output for each input will be:
+
+* `0`: transistor is connected, `0010` (`1`)
+* `1`: transistor is disconnected, `1000` (`z`)
+* `x`: transistor is either closed or opened, `1010` (`1`, `z`)
+
+
+### nmos_transistor()
 Imitates the nmos logic
 
+Since nmos transistors are always conencted to GND (0), we can make assumptions about their logic
+
+The output for each input will be:
+
+* `0`: transistor is disconnected, `1000` (`z`)
+* `1`: transistor is connected, `0001`(`1`)
+* `x`: transistor is either closed or opened, `1001` (`0`, `z`)
+
+## Gate Logic Functions
+
+These functions take initial gate inputs and secondary inputs in a tuple along with the fault input.
+
+The fault input will determine which transistor will be simulated as fault according to the design diagram above the function. We will use `input == 0` to denote no faults. If the input is not within the valid range of transistors, it will be assigned to `0`
+
+These functions will output a tuple `(initial_result, secondary_result)`
--- analysis.py
+++ analysis.py
@@ -1,64 +1,79 @@
-# input -*|
-
+''' input -*| '''
 def pmos_transistor(input):
     match (input) :
-        case "0":
-            return ("1")
+        #0001
+        case 1:
+            return 0b0010 # 1
         
-        case "1":
-            return ("0")
+        #0010
+        case 2:
+            return 0b1000 # z
 
-        case "x":
-            return ("0", "1")
+        #0100
+        case 4:
+            return 0b1010 # 1,z
         
         case default:
             raise Exception("Invalid input to pmos transistor")
 
 
-# input -|
+''' input -| '''
 def nmos_transistor(input):
-    match (input) :
-        case "0":
-            return ("0")
+    match (input):
+        #0001
+        case 1:
+            return 0b1000 # z
         
-        case "1":
-            return ("1")
+        #0010
+        case 2:
+            return 0b0001 # 0
 
-        case "x":
-            return ("0", "1")
+        #0100
+        case 4:
+            return 0b1001 # 0,z
         
         case default:
             raise Exception("Invalid input to nmos transistor")
 
 
+def add_transistor_results(a, b):
+    output = 0
+    if (a & 0b1000): # if a has z, all inputs of b are included in output
+        output |= b
+    if (b & 0b1000): # if b has z, all inputs of a are included in output
+        output |= a
+    output |= a & 0b0100 | a & 0b0100 # if a or b has x, x is in the output.
+    output |= a & b # if a and b both have the same input, it is included
+    if ((a & 0b0010 and b & 0b0001) or (b & 0b0010 and a & 0b0001)): 
+        output |= 0b0100 # if a and b conflict (0, 1) or (1, 0), x is included.
 
-'''
-      VDD
+'''   VDD
        |
    __*| 1
-  |    |
+  |    | <- tran_1_result
 A-|    |_____ Output
-  |    |
+  |    | <- tran_2_result
   |___| 2
        |
-      GND    
-
-
-'''
+      GND    '''
 
 def not_gate_logic(a, fault):
-    match (fault): 
-        case 0:
-            pass
+    #asserting the result after the first transistor
+    if (fault == 1):
+        #transistor 1 is slow, so it will product the same result twice
+        tran_1_result = (pmos_transistor(a[0], a[0]))
+    else: 
+        tran_1_result = (pmos_transistor(a[0], a[1]))
 
-        case 1:
-            pass
-
-        case 2:
-            pass
-
-        case default:
-            raise Exception("NOT only has 2 transistors but fault != {0, 1, 2}")
+    #asserting the result after the second transistor
+    if (fault == 2):
+        #transistor 1 is slow, so it will product the same result twice
+        tran_2_result = (nmos_transistor(a[0], a[0]))
+    else: 
+        tran_2_result = (nmos_transistor(a[0], a[1]))
+    
+    output = (0, 0)
+    
 
 '''
        VDD
