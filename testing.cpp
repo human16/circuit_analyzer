91caf96d7c0e36da7a37b73aaddabd20d50d173a
yesIAmHuman <50881769+human16@users.noreply.github.com>
2025-12-09 11:27:44 -0500

    analyze the number of results given

--- testing.cpp
+++ testing.cpp
@@ -21,29 +21,37 @@ char valToChar(int val) {
 }
 
 //prints the results of the test if they are different from the expected results
-void resultAnalysis(int *results, int *default_results, int a[], int b[], int numFault, int numGate, char *name) {
+int resultAnalysis(int *results, int *default_results, int a[], int b[], int numFault, int numGate, char *name) {
     char buffer1[5];
     char buffer2[5];
     if (memcmp(results, default_results, sizeof(int) * 2) != 0 || (results[1] & (X | Z)) != 0) {
         printf("%s: for values: a:(%c, %c), b:(%c, %c) gate %d fault index: %d -> (%s, %s)\n", name, valToChar(a[0]), valToChar(a[1]), valToChar(b[0]), valToChar(b[1]), (numGate+1), numFault, numToType(results[0], buffer1), numToType(results[1], buffer2));
+        return 1;
     }
+    return 0;
 }
 
 //test the all the possible faults on the given gate
-void runFault(int a[], int b[], int faultArrSize, int *faultArr, int *numFaultsArr, int *(*gate)(int[], int[], int[]), char *name) {
+int runFault(int a[], int b[], int faultArrSize, int *faultArr, int *numFaultsArr, int *(*gate)(int[], int[], int[]), char *name) {
+    int output = 0;
     for(int faultIdx = 0; faultIdx < faultArrSize; faultIdx++) {
         int *default_results = (*gate)(a, b, faultArr);
         for(int numFault = 1; numFault <= numFaultsArr[faultIdx]; numFault++) {
             faultArr[faultIdx] = numFault;
             int *results = (*gate)(a, b, faultArr);
-            resultAnalysis(results, default_results, a, b, numFault, faultIdx, name);
+            output += resultAnalysis(results, default_results, a, b, numFault, faultIdx, name);
         }
         //resetting to array of 0s
         faultArr[faultIdx] = 0;
     }
+    return output;
 }
 
 int main() {
+    int nor_faults = 0;
+    int nand_faults = 0;
+    int nand_xor_faults = 0;
+    int nor_xor_faults = 0;
     for (int a0 = 0; a0 < 2; a0++) {
         for (int a1 = 0; a1 < 4; a1++) {
             int a[] = {vals[a0], vals[a1]};
@@ -65,15 +73,17 @@ int main() {
                     //testing nand xor (using same a & b)
                     int nandFaultArr[4] = {0};
                     int nandNumFaultArr[5] = {4};
-                    runFault(a, b, 4, nandFaultArr, nandNumFaultArr, &nandXorGate, "NAND-XOR");
+                    nand_xor_faults += runFault(a, b, 4, nandFaultArr, nandNumFaultArr, &nandXorGate, "NAND-XOR");
 
                     //testing nor xor 
                     int norFaultArr[5] = {0};
                     int norNumFaultArr[5] = {4};
-                    runFault(a, b, 5, norFaultArr, norNumFaultArr, &norXorGate, "NOR-XOR");
+                    nor_xor_faults += runFault(a, b, 5, norFaultArr, norNumFaultArr, &norXorGate, "NOR-XOR");
                 }
             }
         }
     }
+    printf("NAND-XOR: %d faults\n", nand_xor_faults);
+    printf("NOR-XOR: %d faults\n", nor_xor_faults);
     return 0;
 }
